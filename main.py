697e0b2b9c7db8595a4ce9f99fb43873e4abd011
aldkre <krebs.aldona@op.pl>
2024-10-26 21:54:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ st.set_page_config(
     menu_items={}
 )
 
-def return_text(file):
-    with open(file, 'r', encoding='utf-8') as file:
+def return_text(folder, file):
+    with open(f"{folder}/{file}", 'r', encoding='utf-8') as file:
         caption_text = file.read()
         return caption_text
 
@@ -16,40 +16,50 @@ def return_text(file):
 wybor = st.sidebar.selectbox("Wybierz temat",("Strona główna", "JAVA", "BAZY DANYCH", "ZPI"))
 
 if wybor == "Strona główna":
-    st.balloons()
-    st.write(return_text(".notes/strona glowna.txt"))
+    folder = ".notes"
+    st.write(return_text(folder, file="strona glowna.txt"))
 
 
 elif wybor == "JAVA":
-    # MAIN JAVA SITE
+    folder = ".notes/java"
     st.header("JAVA")
-    # JAVA TABS
     typy_danych, pojecia, metody = st.tabs(["Typy danych", "Podstawowe pojęcia", "Metody"])
 
     with typy_danych:
         typ_prymitywny, typ_referencyjny = st.columns(2)
         with typ_prymitywny:
             st.subheader("Typy prymitywne")
-            st.caption(return_text('.notes/java/typy_prymitywne.txt'))
+            st.caption(return_text(folder, file="typy_prymitywne.txt"))
 
         with typ_referencyjny:
             st.subheader("Typy referencyjne")
-            st.caption(return_text(".notes/java/typy_referencyjne.txt"))
+            st.caption(return_text(folder, file="typy_referencyjne.txt"))
 
         st.divider()
 
-        st.caption(return_text(".notes/java/typy_danych_ogolnie.txt"))
+        st.caption(return_text(folder, file="typy_danych_ogolnie.txt"))
 
     with pojecia:
-        st.caption(return_text(".notes/java/pojecia.txt"))
+        st.caption(return_text(folder, file="pojecia.txt"))
 
     with metody:
-        st.caption(return_text(".notes/java/metody.txt"))
+        st.caption(return_text(folder, file="metody.txt"))
 
 elif wybor == "BAZY DANYCH":
-    typy_baz, inne = st.tabs(["Typy baz danych", "Inne"])
+    folder= ".notes/sql"
+
+    typy_baz, pojecia, operatory, operacje = st.tabs(["Typy baz danych", "Podstawowe pojęcia", "Operatory", "Rodzaje operacji"])
     with typy_baz:
-        st.caption(return_text(".notes/sql/typy_baz_danych.txt"))
+        st.caption(return_text(folder, file="typy_baz_danych.txt"))
+
+    with pojecia:
+        st.caption(return_text(folder, file="pojecia.txt"))
+
+    with operatory:
+        st.caption(return_text(folder, file="operatory.txt"))
+
+    with operacje:
+        st.caption(return_text(folder, file="operacje_sql.txt"))
 
 elif wybor == "ZPI":
     pass

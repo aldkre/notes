7eb2b9f04cef2a1cf705666f52e24a734fd561e4
aldkre <krebs.aldona@op.pl>
2024-11-17 10:04:14 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Java notes",
     page_icon="colapsed",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="collapsed",
     menu_items={}
 )
 
